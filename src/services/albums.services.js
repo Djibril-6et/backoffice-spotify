0f88f4dce3f737529ce17762df17205ea2fa244e
Djibril-6et <djibril.6et@gmail.com>
2024-01-23 22:38:07 +0100

    full project

---
+++ src/services/albums.services.js
@@ -0,0 +1,23 @@
+const API_URL_BASE = 'http://13.49.185.3:9000/api';
+
+export default {
+  getAlbums() {
+    return fetch(`${API_URL_BASE}/album`).then(response => response.json());
+  },
+
+  updateAlbum(id, payload) {
+    return fetch(`${API_URL_BASE}/album/update/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    }).then(res => res.json());
+  },
+
+  deleteAlbum(id) {
+    return fetch(`${API_URL_BASE}/album/delete/${id}`, {
+      method: 'DELETE',
+    }).then(res => res.json());
+  },
+};
